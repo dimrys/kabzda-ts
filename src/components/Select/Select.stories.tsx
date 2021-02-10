7f7bb047530a5dc34d6c89f71dd07cbb7358da9b
dimrys <akkauntdlyraboti@gmail.com>
2021-02-10 16:07:48 +0300

    finished Select component

---
+++ src/components/Select/Select.stories.tsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+
+import {action} from "@storybook/addon-actions"
+import {Story} from "@storybook/react/types-6-0";
+import {Select, SelectPropsType} from "./Select";
+import {Accordion, AccordionTypeProps} from "../Accordion";
+
+
+export default {
+    title: 'Select',
+    component: Select,
+
+};
+
+const onClickSelect = action('select')
+// const Template: Story<SelectPropsType> = (args) => <Select {...args} />;
+//
+// export const WithValue = Template.bind({})
+// WithValue.args = {
+//     value: '2',
+//     items: [{value: '1', title: 'Minsk'}, {value: '2', title: 'Moscow'}, {value: '3', title: 'Kiev'}],
+//     onChange: onClickSelect
+// }
+
+export const WithValue: Story<SelectPropsType> = (args) => {
+    const [value, setValue] = useState('1')
+    return <Select {...args} value={value} onChange={setValue}/>
+};
+WithValue.args = {
+    items: [{value: '1', title: 'Minsk'}, {value: '2', title: 'Moscow'}, {value: '3', title: 'Kiev'}],
+    // onChange: onClickSelect
+}
+
+
+
+export const WithoutValue: Story<SelectPropsType> = (args) => {
+    const [value, setValue] = useState(null)
+    return <Select {...args } value={value} onChange={setValue}/>
+};
+WithoutValue.args = {
+    items: [{value: '1', title: 'Minsk'}, {value: '2', title: 'Moscow'}, {value: '3', title: 'Kiev'}],
+    // onChange: onClickSelect
+}
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,25 +1,75 @@
-import React from 'react';
+import React, {useState, KeyboardEvent, useEffect} from 'react';
+import styles from './Select.module.css';
 
 
 type ItemType = {
     title: string
-    value: any
+    value?: any
 }
 
-type SelectPropsType = {
+export type SelectPropsType = {
     value: any
     onChange: (value: any) => void
     items: ItemType[]
 }
 
 
+export function Select(props: SelectPropsType) {
+    const [active, setActive] = useState(false)
+    const [hoveredElementValue, setHoveredElementValue] = useState(props.value)
 
 
-export function Select(props:SelectPropsType) {
+    useEffect(() => {
+        setHoveredElementValue(props.value)
+    }, [props.value])
+
+
+    const toglItems = () => setActive(!active)
+    const onItemClick = (value:any) => {
+        props.onChange(value);
+        toglItems()
+    }
+    const onKeyUP = (e:KeyboardEvent<HTMLDivElement>) => {
+        if(e.key === "ArrowDown" || e.key === "ArrowUp") {
+            for (let i = 0; i < props.items.length; i++) {
+                if (props.items[i].value === hoveredElementValue) {
+                    const pretendentElement = e.key === "ArrowDown" ? props.items[i +1] : props.items[i - 1]
+                    if (pretendentElement ) {
+                        props.onChange(pretendentElement.value)
+                        return
+                    }
+                }
+            }
+            if (!selectedItem) {
+                props.onChange(props.items[0].value)
+            }
+
+        }
+        if(e.key === "Enter" || e.key === "Escape" ) {
+            setActive(false)
+        }
+
+    }
+
+    const selectedItem = props.items.find(i => i.value === props.value)
+    const hoveredItem = props.items.find(i => i.value === hoveredElementValue)
     return (
-        <div>
-            <div>{}</div>
-            {props.items.map(i => <div>{i.title}</div>)}
+        <div className={`${styles.select}`} onKeyUp={onKeyUP} tabIndex={1}>
+
+            <span className={styles.main} onClick={toglItems}>{selectedItem && selectedItem.title}</span>
+            {
+                active &&
+            <div className={styles.items}>
+                {props.items.map(i => <div
+                    onMouseEnter={() =>{setHoveredElementValue(i.value)}}
+                    className={`${styles.item} ${hoveredItem === i ? styles.selected : ""}`}
+                    onClick={() => {onItemClick(i.value)}}
+                    key={i.value}
+                >{i.title}
+                </div>)}
+            </div>
+            }
+
         </div>
     )
 }
